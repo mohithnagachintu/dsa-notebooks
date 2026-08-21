40fa9648850cbae5b73a0ecae87de86acefed36d
mohithnagachintu <mohithnaga@gmail.com>
2026-08-21 16:58:38 +0530

    Restructured

--- CSES/2_Dynamic_Programming/034_Dice_Combinations.cpp
+++ CSES/2_Dynamic_Programming/034_Dice_Combinations.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 using ll = long long;
-const int MOD = 1e9+7;
+const int MOD = 1e9 + 7;
 
-int solve(int n, vector<int> &dp){
+int solve(int n, vector<int> &dp)
+{
 	// Base case
-	if (n < 0){
+	if (n < 0)
+	{
 		// Not possible
 		return 0;
-	} 
-	if (n == 0){
+	}
+	if (n == 0)
+	{
 		// one possible way
 		return 1;
 	}
@@ -23,8 +26,9 @@ int solve(int n, vector<int> &dp){
 		return dp[n];
 
 	int ans = 0;
-	for (int i=1;i<=6;i++){
-		ans += solve(n-i,dp);
+	for (int i = 1; i <= 6; i++)
+	{
+		ans += solve(n - i, dp);
 		if (ans >= MOD)
 			ans -= MOD;
 	}
@@ -32,7 +36,8 @@ int solve(int n, vector<int> &dp){
 	return ans;
 }
 
-int main(){
+int main()
+{
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
@@ -42,19 +47,21 @@ int main(){
 	// dp.reserve(n);
 	// dp.max_load_factor(0.7);
 
-
 	// Recursion + 1D DP
 	// vector<int> dp(n+1,-1);
 	// int ans = solve(n,dp);
 	// cout << ans << endl;
 
-	vector<int> dp(n+1,0);
+	vector<int> dp(n + 1, 0);
 	dp[0] = 1;
-	for (int x=1;x<=n;x++){
+	for (int x = 1; x <= n; x++)
+	{
 		int ans = 0;
-		for (int i=0;i<=6;i++){
-			if (x-i >= 0){
-				ans += dp[x-i];
+		for (int i = 0; i <= 6; i++)
+		{
+			if (x - i >= 0)
+			{
+				ans += dp[x - i];
 				if (ans >= MOD)
 					ans -= MOD;
 			}
